dbb9ba340af69c371d0b2fc0d666618a842e048b
adilhan <Adilhansatymkulov40@gmail.com>
2026-04-04 14:13:49 +0600

    fix

--- BilimAI/celery.py
+++ BilimAI/celery.py
@@ -1,9 +1,9 @@
-import os
+# import os
 
-from celery import Celery
+# from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BilimAI.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BilimAI.settings")
 
-app = Celery("BilimAI")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+# app = Celery("BilimAI")
+# app.config_from_object("django.conf:settings", namespace="CELERY")
+# app.autodiscover_tasks()
